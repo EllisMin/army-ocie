16aab045510dc210d325976f0c16ba2c5ff48633
ellismin <dbmin@hotmail.com>
2020-08-09 21:56:24 -0500

    [ADD]: lazy load images

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,17 @@
 import React from "react";
+import LazyLoad from "react-lazyload";
 
 import "./styles.scss";
 
 const Card = ({ title, imageSrc, missing, note }) => {
   return (
-    <div className="card">
-      <h4 className={`${missing ? "red-color" : ""}`}>{title}</h4>
-      <img className="img-item" src={imageSrc} alt="img" />
-      {note && <span className="note">{`${note}`}</span>}
-    </div>
+    <LazyLoad>
+      <div className="card">
+        <h4 className={`${missing ? "red-color" : ""}`}>{title}</h4>
+        <img className="img-item" src={imageSrc} alt="img" />
+        {note && <span className="note">{`${note}`}</span>}
+      </div>
+    </LazyLoad>
   );
 };
 
